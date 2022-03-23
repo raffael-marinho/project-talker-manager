298f5e7b70eeaa5283a982d2f089866a158c47ad
raffael marinho <silvamarinho10@gmail.com>
2022-03-23 11:20:27 -0300

    feat:add search by parameter

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -8,13 +8,31 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 router.get('/talker', async (_request, response) => {
-  const talker = await fs.readFile('talker.json');
+  const talker = await fs.readFile('talker.json', 'utf-8');
+  console.log(talker);
   const talkerObj = JSON.parse(talker);
+  console.log(talkerObj);
   if (!talkerObj) {
     return response.status(HTTP_OK_STATUS).send([]);
   }
   return response.status(HTTP_OK_STATUS).send(talkerObj);
+});
+
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('talker.json', 'utf-8');
+  const talkersObj = JSON.parse(talkers);
+  const talker = talkersObj.find((talkerObj) => talkerObj.id === parseInt(id, 20));
+
+  if (!talker) {
+    return res.status(HTTP_NOT_FOUND_STATUS).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+
+  return res.status(HTTP_OK_STATUS).send(talker);
 });
 
 module.exports = router;
